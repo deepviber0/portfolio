bbf53a6d086e64dba787f67f1365c7e7ed7568a1
Shaik Umar Althaf <deepcoders0@gmail.com>
2025-10-30 11:28:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/",
+  base: "",
   server: {
     host: "::",
     port: 8080,
